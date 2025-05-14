4fdafb56184d6fe0a68799dd288caa4d29d8b1eb
Willyan <willeste25@gmail.com>
2025-05-14 12:55:23 +0700

    Update

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -13,5 +13,11 @@ $routes->get('/Tos', 'Page::tos');
 $routes->get('/Biodata', 'Page::biodata');
 $routes->setAutoRoute(false);
 
+
+$routes->get('/', 'Pages::index');
+$routes->get('/books', 'Books::index');
+$routes->get('/books/create', 'Books::create');
+$routes->get('/books/edit(:segment)', 'Books::edit$1');
+$routes->get('/books/save', 'Books::save');
 $routes->get('/books', 'Books::index');
 $routes->get('/books/(:segment)', 'Books::detail/$1');
